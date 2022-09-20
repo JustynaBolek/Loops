76793ccc29880082b99f9c6de2705f7fc9cd0dd3
JustynaBolek <justyna.bolek@gmail.com>
2022-09-20 20:38:01 +0200

    added switch

---
+++ src/Switch.java
@@ -0,0 +1,13 @@
+public class Switch {
+    public static void main(String[] args) {
+        int x = 2;
+        switch (x){
+            case 1:
+                System.out.println("Justyna jest super");
+                break;
+            case 2:
+                System.out.println("Justyna jest mega super");
+                break;
+        }
+    }
+}
